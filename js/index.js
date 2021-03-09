1a1a4bf487d2061d9ff43a7db69c3d0a61f8d864
NARPAT PATEL <56156133+narpatpatel03191@users.noreply.github.com>
2021-03-09 12:08:44 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB0IolPCQhnFsBsj386_lhACoYPITXtikw",
+    authDomain: "helping-hands-f52f1.firebaseapp.com",
+    projectId: "helping-hands-f52f1",
+    storageBucket: "helping-hands-f52f1.appspot.com",
+    messagingSenderId: "130421144161",
+    appId: "1:130421144161:web:2f70ceb214e1610ca44820",
+    measurementId: "G-B2KXQ7CK7L"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics(); 
